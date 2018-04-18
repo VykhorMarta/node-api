4a80242094d09c2349e22c972ed4de3d16f0a1fe
Vykhor Marta <vykhor.m@gmail.com>
2018-04-18 18:54:25 +0300

    second commit

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,3 +1,4 @@
+const User = require("../models/user")
 const mongoose = require('mongoose');
 const express = require('express');
 
@@ -10,4 +11,36 @@ router.get("/", (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const user = new User({
+        _id: mongoose.Types.ObjectId(),
+        name: req.body.name
+        });
+    user.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            "message": "Handling POST request to users",
+            "createdUser": result
+        })
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+            
+        });
+    });
+
+})
+
+router.get('/:userId', (req,res)=> {
+    const _id= req.params.userId;
+    User.findById(_id).then(userData => {
+        console.log(userData);
+        res.status(200).json(userData)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err})
+    });
+})
 module.exports = router;
+
